9a51f88f4edfdf173d5faab38d25654a0c0912a9
Julio Martin <juliomartinpozo@gmail.com>
2017-02-03 00:15:32 +0100

    added english and spanish, pending dog module components

--- src/app/dogs/dog.service.ts
+++ src/app/dogs/dog.service.ts
@@ -6,14 +6,29 @@ import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/do';
 
 import { IDog } from './dog';
-
+import {TranslateService, LangChangeEvent} from 'ng2-translate';
 
 @Injectable()
 export class DogService {
 
+    translate: TranslateService;
+
     private _dogUrl = './api/dogs/dogs.json';
 
-    constructor(private _http: Http) {}
+    private _dogENUrl = './api/dogs/dogs_en.json';
+
+    private _dogESUrl = './api/dogs/dogs_es.json';
+
+    constructor(private _http: Http, _translate : TranslateService) {
+        //TODO Retrieve dogs information according to current language
+        /*this.translate = _translate;
+        this.translate.onLangChange.subscribe((event: LangChangeEvent) => {
+            console.log(event.lang ==='es' ? this._dogESUrl : this._dogENUrl);
+            this._dogUrl = event.lang ==='es' ? this._dogESUrl : this._dogENUrl;
+            this.getDogs();
+        });*/
+    }
+
 
     getDogs() : Observable<IDog[]> {
         return this._http.get(this._dogUrl)
