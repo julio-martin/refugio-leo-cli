31ba9019e0d5234efaff8bf9f2302fc08a9eb710
Julio Martin <juliomartinpozo@gmail.com>
2017-03-09 20:03:44 +0100

    added formspree to contact form

--- src/app/contact-form.component.ts
+++ src/app/contact-form.component.ts
@@ -4,6 +4,7 @@ import { ContactInfo }    from './contact-info';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import {CustomValidators} from './custom.validator';
 import {TranslateService} from 'ng2-translate';
+import { Http, RequestOptions, Headers} from '@angular/http';
 
 @Component({
   selector: 'contact-form',
@@ -17,7 +18,7 @@ export class ContactFormComponent implements OnInit{
   subject : string;
   message : string;
 
-  constructor(private _fb: FormBuilder, private _translate: TranslateService) { }
+  constructor(private _fb: FormBuilder, private _translate: TranslateService, private _http:Http) { }
 
   ngOnInit() {
     if(this.dog) {
@@ -39,5 +40,12 @@ export class ContactFormComponent implements OnInit{
 
   onSubmit({ value, valid }: { value: ContactInfo, valid: boolean }) {
     this.submitted = true;
+    let body = JSON.stringify(value);
+    console.log(body);
+    let headers = new Headers({ 'Content-Type': 'application/json', 'Accept':'application/json' });
+    let options = new RequestOptions({ headers: headers });
+    this._http.post('https://formspree.io/contacto@elrefugiodeleo.com', body, options).subscribe(result => {
+        console.log( result );
+    });
   }
 }
